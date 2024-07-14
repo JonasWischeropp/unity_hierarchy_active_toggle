1b53599f135bfabcb543880078fa4581383ffc9c
Jonas Wischeropp <jonas@wischeropp.de>
2024-07-14 15:24:46 +0200

    fix save is no longer called every frame

--- Editor/ActiveToggleSettings.cs
+++ Editor/ActiveToggleSettings.cs
@@ -18,13 +18,29 @@ namespace JonasWischeropp.Unity.EditorTools.Hierarchy {
             var keywords = new string[]{"Hierarchy", "Alignment"};
             return new SettingsProvider("Preferences/Hierarchy/Active Toggle", SettingsScope.User, keywords) {
                 guiHandler = (searchContext) => {
-                    instance.Enabled = EditorGUILayout.Toggle(
-                        new GUIContent("Active", "Enable/Disable the package"), instance.Enabled);
-                    instance.Alignment = (ToggleAlignment)EditorGUILayout.EnumPopup(
-                        new GUIContent("Alignment", "Change alignment"), instance.Alignment);
-                    instance.Save(true);
+                    instance.SetEnabled(EditorGUILayout.Toggle(
+                        new GUIContent("Active", "Enable/Disable the package"), instance.Enabled));
+                    instance.SetAlignment((ToggleAlignment)EditorGUILayout.EnumPopup(
+                        new GUIContent("Alignment", "Change alignment"), instance.Alignment));
                 }
             };
+        }
+        
+        private void SaveWhenDifferent<T>(T oldValue, T newValue) {
+            if (!oldValue.Equals(newValue))
+                Save(true);
+        }
+        
+        private void SetEnabled(bool enabled) {
+            var old = Enabled;
+            Enabled = enabled;
+            SaveWhenDifferent(old, Enabled);
+        }
+        
+        private void SetAlignment(ToggleAlignment alignment) {
+            var old = Alignment;
+            Alignment = alignment;
+            SaveWhenDifferent(old, Alignment);
         }
     }
 
