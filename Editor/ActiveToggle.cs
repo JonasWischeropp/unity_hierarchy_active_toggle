6e3d30ec18ba0c0d210dcf2908005795fc0285b3
Jonas Wischeropp <jonas@wischeropp.de>
2025-07-12 18:21:22 +0200

    fix tickbox position in search mode

--- Editor/ActiveToggle.cs
+++ Editor/ActiveToggle.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,7 +9,10 @@ namespace JonasWischeropp.Unity.EditorTools.Hierarchy {
     internal class ActiveToggle {
         private static readonly Color prefabModifiedColor = new Color(0.05882353f, 0.5058824f, 0.7450981f); // 8AD9FF
         private static readonly Color backgroundColor = new Color(0.2196078f, 0.2196078f, 0.2196078f); // 383838
-        
+
+        static Type hierarchyWindowType = Type.GetType("UnityEditor.SceneHierarchyWindow, UnityEditor");
+        static PropertyInfo searchFilterProperty = hierarchyWindowType.GetProperty("searchText");
+
         static ActiveToggle() {
             EditorApplication.hierarchyWindowItemOnGUI += Draw;
         }
@@ -20,7 +25,18 @@ namespace JonasWischeropp.Unity.EditorTools.Hierarchy {
             if (go == null)
                 return;
 
-            int parentCount = ActiveToggleSettings.instance.Alignment == ToggleAlignment.Left ? CountParents(go.transform) : 0;
+            var window = Resources.FindObjectsOfTypeAll(hierarchyWindowType)[0];
+            string searchFilter = searchFilterProperty.GetValue(window) as string;
+            bool searching = searchFilter.Length > 0;
+
+            int parentCount;
+            if (searching)
+                parentCount = -1;
+            else if (ActiveToggleSettings.instance.Alignment == ToggleAlignment.Left)
+                parentCount = CountParents(go.transform);
+            else
+                parentCount = 0;
+
             float xPosition = CalculateRectXValue(selectionRect, parentCount + 1);
 
             // Redraw the line indicating a prefab that was modified because it is under the toggle box now
@@ -48,11 +64,11 @@ namespace JonasWischeropp.Unity.EditorTools.Hierarchy {
             }
 
             GUI.color = oldColor;
-            
+
             if (ActiveToggleSettings.instance.MiddleClickToggle)
                 HandleMiddleMouseClick(selectionRect, go);
         }
-        
+
         private static void HandleMiddleMouseClick(Rect selectionRect, GameObject gameObject) {
             bool middleMouseButtonPressed = Event.current.button == 2
                 && Event.current.type == EventType.MouseDown;
@@ -60,12 +76,12 @@ namespace JonasWischeropp.Unity.EditorTools.Hierarchy {
                 ToggleGameObjectActiveState(gameObject);
             }
         }
-        
+
         private static void ToggleGameObjectActiveState(GameObject gameObject) {
             Undo.RecordObject(gameObject, gameObject.activeSelf ? "Disable Object" : "Enable Object");
             gameObject.SetActive(!gameObject.activeSelf);
         }
-        
+
         // Incase that this leads to performance issues:
         // It would be possible to cache the last object for each level
         // and then look up the parent to get its level.
